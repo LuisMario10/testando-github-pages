f495fbe3973cc978606daad1652be3358f483efd
Luis Mario <luismario337y@gmail.com>
2024-12-13 21:22:13 -0300

    feat

--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
 const body = document.body;
 const button = document.querySelector('#button-change-theme');
+const buttonMenuBurguer = document.querySelector('#button-menu-burguer');
+const menuResponsive = document.querySelector('#menu-responsive')
+
+let menuIsActivate = false;
 
 button.addEventListener("click", () => {
     
@@ -8,4 +12,17 @@ button.addEventListener("click", () => {
     
     if(body.classList.contains('dark-mode')) button.innerHTML = '&#x2600;'
     else button.innerHTML = '&#x1F319;'
+});
+
+buttonMenuBurguer.addEventListener("click", () => {
+    
+    if(!menuIsActivate) { 
+        buttonMenuBurguer.style.cssText = "rotate: 180deg";
+        menuIsActivate = true;
+    } else {
+        buttonMenuBurguer.style.cssText = "rotate: 90deg";
+        menuIsActivate = false;
+    }
+    menuResponsive.classList.toggle('hidden');
+    menuResponsive.classList.toggle('menu-responsive');
 });
